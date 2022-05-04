998e70e5fab7aa733901c2ffef884bee51badbbb
Francisco Bas <franbjalon@gmail.com>
2022-05-04 16:00:49 +0100

    Add compresion

--- TX_RX2/read_pen.sh
+++ TX_RX2/read_pen.sh
@@ -6,4 +6,5 @@ destination_path=.
 pen_name=`ls -l /dev/disk/by-uuid/ | grep -o sda.*`
 sudo mount /dev/$pen_name /media/usb -o uid=pi,gid=pi
 cp $pen_path/$1 $destination_path/$1 
+tar czvf  $1 $1 
 sudo umount /media/usb
