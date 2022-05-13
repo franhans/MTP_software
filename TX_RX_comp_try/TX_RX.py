0d2d06bb7d92441a2b2875cb5d2fafbfecf83947
Francisco Bas <franbjalon@gmail.com>
2022-05-13 10:55:23 +0100

    Add infinite loop

--- TX_RX_comp_try/TX_RX.py
+++ TX_RX_comp_try/TX_RX.py
@@ -10,22 +10,12 @@ import gzip
 import RPi.GPIO as GPIO
 
 
-def progressBar(count, total):
-    bar_len = 60
-    filled_len = int(round(bar_len*count/float(total)))
-
-    percents = round(100.0 * count / float(total), 1)
-    bar = "=" * filled_len + "-" * (bar_len - filled_len)
-
-    sys.stdout.write("[%s] %s%s\r" % (bar, percents, '%'))
-    sys.stdout.flush()
-
 def transmit():
 
     pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
 
-    # SPI initialization
-    radio = NRF24(GPIO, spidev.SpiDev()) #SPI configuration
+    #NRF24 Initialization
+    radio = NRF24(GPIO, spidev.SpiDev())
     radio.begin(0, 17)
     time.sleep(0.5)
     radio.setRetries(15,15)
@@ -47,9 +37,8 @@ def transmit():
     led_counter = 0
     led = True
 
-    print("Transceiver initialized")
-
-    print(radio.getCRCLength())
+    #print("Transceiver initialized")
+    #print(radio.getCRCLength())
 
     # Start opening the file
     # Gets input file, if no fie has been specified, default file "test.txt" is used
@@ -58,14 +47,13 @@ def transmit():
     else:
         filename = "test.txt"
 
-    print("Name of the text file: ", filename)
+    #print("Name of the text file: ", filename)
 
-    
     with open(filename, 'rb') as f_in:
         with gzip.open('transmit.txt.gz', 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
 
-    # Loads input file as an array of bytes
+    # Save the compressed file in an array of bytes
     file = open('transmit.txt.gz', mode='rb', buffering=0)
     data = bytearray()
     while(True):
@@ -75,16 +63,14 @@ def transmit():
         data += d
     file.close()
 
-
     size = len(data)
     #print("Size in bytes---->>>>>> ", size)
 
     while(True and not(GPIO.input(16))):
-        start_time = time.time()
 
         payload_length = 29
         num_packets = math.ceil(size/payload_length)
-        currentPacket = 0
+        id_packet = 0
         counter=0
         timeout=0.1
 
@@ -94,18 +80,16 @@ def transmit():
             dataSize = min(payload_length, size-counter*payload_length)
 
             # Check if the packet is the last one
-            #packet = [int(0x00)] if (counter == (num_packets - 1)) else packet = [int(0xFF)]
             if counter == (num_packets - 1):
                 packet = [int(0x00)] #we are sending the last packet
                 #print("Las packet sent")
             else:
                 packet = [int(0xFF)] #the packet is not the last one
 
-            packet.append(int(0xFF & (currentPacket))) # Add the id of the packet
+            packet.append(int(0xFF & (id_packet))) # append the id of the packet
+            packet.append(int(dataSize)) # append the payload length
 
-            packet.append(int(dataSize)) # Add the payload length
-
-            for i in range(payload_length): # Add the data in the payload
+            for i in range(payload_length): # append the data in the payload
                 # If data size is less than payload size, fill with 0
                 if i < dataSize:
                     packet.append(int(data[counter*payload_length+i]))
@@ -131,42 +115,27 @@ def transmit():
                             #print("ack_size: ", ack_size)
                             radio.read(ack, 1) #the ACK are always 32 bytes
                             #print("ACK_value: ", ack)
-                            ack_id = (0xFF & ack[0]) 
-                            if ack_id == currentPacket:
+                            ack_id = (0xFF & ack[0])
+                            if ack_id == id_packet:
                                 retransmit = False
                                 break
                                 #print('---------------Received = Type')
                     time.sleep(0.00001)
 
-
                 radio.stopListening()
-	        
-                led_counter = led_counter + 1
-                if  led_counter == 50:
-                    led_counter = 0
-                    if led:
-                        GPIO.output(15,GPIO.HIGH)
-                        led = False
-                    else:
-                        GPIO.output(15,GPIO.LOW)
-                        led = True
 
-            currentPacket = (currentPacket + 1)%250
+            id_packet = (id_packet + 1)%250
             counter = counter + 1
-            progressBar(counter, num_packets)
 
-            #if counter == num_packets:
-            #    break
-
-        transfer_time = time.time() - start_time
-        if transfer_time > 60:
-            print("Total time:"),
-            print(transfer_time/60),
-            print("min")
-        else:
-            print("Total time:"),
-            print(transfer_time),
-            print("seconds")
+            led_counter = led_counter + 1
+            if  led_counter == 50:
+                led_counter = 0
+                if led:
+                    GPIO.output(15,GPIO.HIGH)
+                    led = False
+                else:
+                    GPIO.output(15,GPIO.LOW)
+                    led = True
 
 
 def receive():
@@ -191,11 +160,11 @@ def receive():
     radio2.stopListening()
     radio2.printDetails()
 
-    error = True 
+    error = True
     while(error and not GPIO.input(16)):
         payload_length = 29
         id_expected=0
-        counter=0 #number of received packets
+        counter=0 #number of total received packets
         last_packet = False
         data = [] # Array to store the whole data
 
@@ -204,10 +173,8 @@ def receive():
         led = True
 
         radio2.startListening()
-
         try:
             while (not last_packet):
-                #pipe = [0]
                 while not radio2.available() and not GPIO.input(16):
                     time.sleep(0.00001)
 
@@ -215,7 +182,7 @@ def receive():
                 packet = []
                 radio2.read(packet, 32)
 
-                packet_id = packet[1]
+                packet_id = packet[1] #get the type of packet
                 #print("packet_id: ", packet_id)
 
                 #Generate the ACK
@@ -225,7 +192,7 @@ def receive():
                 radio2.write(ack)
                 time.sleep(0.00001)
                 radio2.startListening()
-                
+
                 led_counter = led_counter + 1
                 if  led_counter == 50:
                     led_counter = 0
@@ -241,7 +208,7 @@ def receive():
 
                     size = packet[2] # Get the size of the data in the payload
                     #print("Size:", size)
-	    	
+
                     l = len(data)
 
                     # Check if current data buffer is large enough to store the received data
@@ -256,7 +223,7 @@ def receive():
                         data[counter*payload_length + i] = packet[i + 3]
 
                     # If the received packet is the last one, write the buffer to a file and exit
-                    if GPIO.input(16) or (packet[0] == 0x00):
+                    if (packet[0] == 0x00):
                         last_packet = True
                         file = open("received_compressed.txt", mode="wb")
                         file.write(bytearray(data))
@@ -265,7 +232,7 @@ def receive():
                         with gzip.open('received_compressed.txt', 'rb') as f:
                             uncompressed_sentences = f.read()
 
-                        uncompressedFile = open('received.txt', 'wb') 
+                        uncompressedFile = open('MTP-S22-SRI-A-RX.txt', 'wb')
                         uncompressedFile.write(uncompressed_sentences)
                         error = False
 
@@ -273,78 +240,81 @@ def receive():
                     id_expected = (id_expected+1)%250
                     counter = counter+1
 
-                elif GPIO.input(16):
-                    file = open("received.txt", mode="wb")
+                if GPIO.input(16):
+                    file = open("MTP-S22-SRI-A-RX.txt", mode="wb")
                     file.write(bytearray(data))
                     file.close()
                     last_packet = True
 
 
         except:
-            print("exception")
+            #print("exception")
             error = True
             pass
 
 
     GPIO.cleanup()
 
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(14, GPIO.OUT)#LED ON_OFF
-GPIO.setup(15, GPIO.OUT)#LED TX
-GPIO.setup(18, GPIO.OUT)#LED RX
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)#ON_OFF
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)#TX_RX
-
-GPIO.output(14,GPIO.LOW)
-GPIO.output(15,GPIO.LOW)
-GPIO.output(18,GPIO.LOW)
-
-
-if GPIO.input(20): # If SW2 'ON' then module is TX
-
-    while GPIO.input(16) == True:
-        time.sleep(0.1)
-
-    GPIO.output(14,GPIO.HIGH)#system is ON
-    print("ON")
-
-    GPIO.output(18,GPIO.HIGH)#module is RX
-    print("RX")
-    receive()
-    print("recieved")
-    # set all the leds on
+while(True):
     GPIO.setmode(GPIO.BCM)
+
     GPIO.setup(14, GPIO.OUT)#LED ON_OFF
     GPIO.setup(15, GPIO.OUT)#LED TX
     GPIO.setup(18, GPIO.OUT)#LED RX
-    GPIO.output(15,GPIO.HIGH)
-    GPIO.output(14,GPIO.HIGH)
-    GPIO.output(18,GPIO.HIGH)
-    os.system('./write_pen.sh received.txt')
-    print("written")
+    GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)#ON_OFF
+    GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)#TX_RX
 
-else: # If SW2 'OFF' then module is RX
-
-    GPIO.output(15,GPIO.HIGH)
-    GPIO.output(14,GPIO.HIGH)
-    GPIO.output(18,GPIO.HIGH)
-    os.system('./read_pen.sh test.txt')
-    GPIO.output(15,GPIO.LOW)
     GPIO.output(14,GPIO.LOW)
+    GPIO.output(15,GPIO.LOW)
     GPIO.output(18,GPIO.LOW)
 
-    while GPIO.input(16) == True:
+    while GPIO.input(16) == False:
         time.sleep(0.1)
 
-    GPIO.output(14,GPIO.HIGH)#system is ON
-    print("ON")
+    if GPIO.input(20): # If SW2 'ON' then module is RX
 
-    GPIO.output(15,GPIO.HIGH)#module is TX
-    print("TX")
-    transmit()
- # If SW1 'OFF' system reset
+        while GPIO.input(16) == True:
+            time.sleep(0.1)
 
-GPIO.output(14,GPIO.LOW)
-GPIO.output(15,GPIO.LOW)
-GPIO.output(18,GPIO.LOW)
+        GPIO.output(14,GPIO.HIGH)#system is ON
+        #print("ON")
+
+        GPIO.output(18,GPIO.HIGH)#module is RX
+        #print("RX")
+        receive()
+        #print("recieved")
+        # set all the leds on
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(14, GPIO.OUT)#LED ON_OFF
+        GPIO.setup(15, GPIO.OUT)#LED TX
+        GPIO.setup(18, GPIO.OUT)#LED RX
+        GPIO.output(15,GPIO.HIGH)
+        GPIO.output(14,GPIO.HIGH)
+        GPIO.output(18,GPIO.HIGH)
+        os.system('./write_pen.sh MTP-S22-SRI-A-RX.txt')
+        #print("written")
+
+    else: # If SW2 'OFF' then module is TX
+
+        GPIO.output(15,GPIO.HIGH)
+        GPIO.output(14,GPIO.HIGH)
+        GPIO.output(18,GPIO.HIGH)
+        os.system('./read_pen.sh test.txt')
+        GPIO.output(15,GPIO.LOW)
+        GPIO.output(14,GPIO.LOW)
+        GPIO.output(18,GPIO.LOW)
+
+        while GPIO.input(16) == True:
+            time.sleep(0.1)
+
+        GPIO.output(14,GPIO.HIGH)#system is ON
+        #print("ON")
+
+        GPIO.output(15,GPIO.HIGH)#module is TX
+        #print("TX")
+        transmit()
+     # If SW1 'OFF' system reset
+
+    GPIO.output(14,GPIO.LOW)
+    GPIO.output(15,GPIO.LOW)
+    GPIO.output(18,GPIO.LOW)
---
+++ TX_RX_comp_try_old/read_pen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pen_path=/media/usb
+destination_path=.
+
+
+while [ "$is_pen_connected" = "" ]; do
+    is_pen_connected=`ls -l /dev/disk/by-uuid/ | grep -o sda.*`
+    sleep 1
+done
+
+pen_name=`ls -l /dev/disk/by-uuid/ | grep -o sda.*`
+sudo mount /dev/$pen_name /media/usb -o uid=pi,gid=pi
+sleep 2
+file_name=$(ls /media/usb | grep .*.txt)
+cp $pen_path/$file_name $destination_path/$1
+sudo umount /media/usb
+
